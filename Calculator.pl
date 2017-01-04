f973e9de6e86de7d7daee91b6c34bb944606b312
Tzeroxik <Tzeroxik@users.noreply.github.com>
2017-01-04 21:07:42 +0000

    Update Calculator.pl

--- Calculator.pl
+++ Calculator.pl
@@ -1,8 +1,8 @@
 %calculates with any simple operation, or combination of simple operations.
-calculator(Result):- read(In),calculate(In,Result).
+calculator(Result):- read(In), calculate(In,Result).
 calculate(X + Y, R):- R is X + Y.
 calculate(X - Y, R):- R is X - Y.
 calculate(X * Y, R):- R is X * Y.
 calculate(X / Y, R):- R is X / Y.
 calculate(X ^ Y, R):- R is X ^ Y.
-calculate(X,X).
+calculate(X, X).
