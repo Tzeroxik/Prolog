c329124dd37fe9cc3457feb180292d6a19c8bf5c
Tzeroxik <Tzeroxik@users.noreply.github.com>
2017-01-04 15:25:20 +0000

    Update and rename RangedList.pl to lists.pl

--- RangedList.pl
+++
@@ -1,5 +0,0 @@
-
-createListFromRange(Min,Max,List):- addElement(Min,Max,[],List).
-addElement(Min,Max,_,[]):- Min > Max.
-addElement(Min,Max,List,[Max|List]):- Min = Max. 
-addElement(Min,Max,List,ListF):- N is Max-1, addElement(Min, N, [Max|List],ListF).
---
+++ lists.pl
@@ -0,0 +1,12 @@
+%(1)% Creates one list with [Min, Max] domain, with the following sequence (Min,Min+1,Min+2,...,Max). 
+listFromRange(Min,Max,List):- addElement(Min,Max,[],List).
+addElement(Min,Max,_,[]):- Min > Max.
+addElement(Min,Max,List,[Max|List]):- Min =:= Max. 
+addElement(Min,Max,List,ListF):- N is Max-1, addElement(Min, N, [Max|List],ListF).
+
+%(2)% Creates any number of random int lists with [0, RN] domain.
+randomIntList(RN,List) :- random(X),setNumberRange(X,RN,Range), addElement(0,Range,RN,[],List).
+addElement(N,R,RN,_,List) :- N > R, randomIntList(RN,List).
+addElement(N,R,_,List,List) :- N =:= R.
+addElement(N,R,RN,List,FList) :- N1 is N+1, random(X),setNumberRange(X,RN,E) ,append(List,[E],NList), addElement(N1,R,RN,NList,FList).
+setNumberRange(X,R,Y):- N is R * X, Y is round(N). 
