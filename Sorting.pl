27ba7ef8145099a57349335bece0ad7fb0500977
Tzeroxik <Tzeroxik@users.noreply.github.com>
2017-01-04 20:44:09 +0000

    Create Sorting.pl

---
+++ Sorting.pl
@@ -0,0 +1,11 @@
+%mergesort algorythm implementation
+mergeSort([],[]).
+mergeSort([E],[E]).
+mergeSort(Lista, ListaOrdenada) :- separate(Lista,Ll,Lr), mergeSort(Lr, LrF), mergeSort(Ll,LlF), join(LlF, LrF, ListaOrdenada).
+separate([],[],[]).
+separate([E],[E],[]).
+separate([C,E|R],[C|L1],[E|L2]):- separate(R,L1,L2).
+join(L,[],L).
+join([],L,L).
+join([C1|R1],[C2|R2] ,[C1|Lf]) :- C1 =< C2, join(R1,[C2|R2],Lf).
+join([C1|R1],[C2|R2] ,[C2|Lf]) :- C1 > C2, join(R2,[C1|R1],Lf).
