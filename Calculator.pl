5dd1d2227538e3538d1463444840329e5de3113d
Tzeroxik <Tzeroxik@users.noreply.github.com>
2017-01-05 11:48:20 +0000

    Delete Calculator.pl

--- Calculator.pl
+++
@@ -1,8 +0,0 @@
-%calculates with any basic arithmetic operation, or combination of basic operations (and powers).
-calculator(Result):- read(In), calculate(In,Result).
-calculate(X + Y, R):- R is X + Y,!.
-calculate(X - Y, R):- R is X - Y,!.
-calculate(X * Y, R):- R is X * Y,!. 
-calculate(X / Y, R):- R is X / Y,!. 
-calculate(X ^ Y, R):- R is X ^ Y,!.
-calculate(X, X).
