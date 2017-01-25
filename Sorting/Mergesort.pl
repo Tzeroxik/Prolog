cd54aa4062bc8395534c46d1ea75eb16ae213ed1
Tzeroxik <Tzeroxik@users.noreply.github.com>
2017-01-25 00:25:49 +0000

    Update Mergesort.pl

--- Sorting/Mergesort.pl
+++ Sorting/Mergesort.pl
@@ -8,4 +8,4 @@ separate([C,E|R],[C|L1],[E|L2]):- separate(R,L1,L2).
 join(L,[],L).
 join([],L,L).
 join([C1|R1],[C2|R2] ,[C1|Lf]) :- C1 =< C2, join(R1,[C2|R2],Lf).
-join([C1|R1],[C2|R2] ,[C2|Lf]) :- C1 > C2, join(R2,[C1|R1],Lf).
+join(L,[C2|R2] ,[C2|Lf]) :- join(R2,L,Lf).
